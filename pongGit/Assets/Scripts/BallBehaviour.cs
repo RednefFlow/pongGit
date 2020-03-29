8dc0c83183d06c9da2294e915bd78335fd366ead
Florian <jumeaux_flo@yahoo.fr>
2020-03-29 15:31:09 +0200

    Import 1er projet

---
+++ pongGit/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehaviour : MonoBehaviour
+{
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Vector3 endMarker;
+
+    // Movement speed in units per second.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+    void Start()
+    {
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+        endMarker = new Vector3(-20.0f, Random.Range(-4.0f,4.0f),0.0f);
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(this.transform.position, endMarker);
+    }
+
+    // Move to the target end position.
+    void Update()
+    {
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed equals current distance divided by total distance.
+        float fractionOfJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(startMarker.position, endMarker, fractionOfJourney);
+    }
+}
---
+++ pongGit/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        if(speed == 1.0f){
+
+        }
+    }
+}
